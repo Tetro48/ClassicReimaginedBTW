38d8481c3a4f6ff4d39145fc48776c8aeb0dbecc
Tetro48 <76738929+Tetro48@users.noreply.github.com>
2025-05-31 13:27:21 +0700

    v0.7.2: Faster and lenient fishing - Bite rate is increased by 2x overall (full moons are 16x over the BTW base) - Tightest catch is now one second, instead of half a second

---
+++ src/main/java/net/tetro48/classicaddon/mixin/entity/EntityFishHookMixin.java
@@ -0,0 +1,25 @@
+package net.tetro48.classicaddon.mixin.entity;
+
+import net.minecraft.src.EntityFishHook;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EntityFishHook.class)
+public class EntityFishHookMixin {
+	@Shadow private int ticksCatchable;
+
+	@ModifyArg(method = "onUpdate", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I", ordinal = 1))
+	private int reduceRandomRange(int bound) {
+		return bound - 10;
+	}
+	@Inject(method = "onUpdate", at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/src/EntityFishHook;playSound(Ljava/lang/String;FF)V"))
+	private void moreLenientCatches(CallbackInfo ci) {
+		this.ticksCatchable += 10;
+	}
+	@ModifyArg(method = "checkForBite", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I"))
+	private int lowerTimeToBite(int bound) {
+		return bound / 2;
+	}
+}
