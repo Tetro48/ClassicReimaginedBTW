4bf8d7136f35e119e91bb1c5413aa73f1adc1395
Tetro48 <76738929+Tetro48@users.noreply.github.com>
2025-06-08 09:10:32 +0700

    v0.8.4: Lazily fixed leaky compass coordinate when getting kicked/disconnected

---
+++ src/main/java/net/tetro48/classicaddon/mixin/GuiMultiplayerMixin.java
@@ -0,0 +1,16 @@
+package net.tetro48.classicaddon.mixin;
+
+import btw.community.classicaddon.ClassicAddon;
+import net.minecraft.src.GuiMultiplayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GuiMultiplayer.class)
+public abstract class GuiMultiplayerMixin {
+	@Inject(method = "initGui", at = @At("RETURN"))
+	private void disableCompassCoordsJustInCase(CallbackInfo ci) {
+		ClassicAddon.isServerRunningThisAddon = false;
+	}
+}
