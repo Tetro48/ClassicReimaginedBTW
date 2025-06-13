f62f27b8b0b2ec57e6d76ac63ba3ee17ea7f137a
Tetro48 <76738929+Tetro48@users.noreply.github.com>
2025-06-13 19:21:01 +0700

    Implement anti-leak system into the new moon modification

--- src/main/java/net/tetro48/classicaddon/mixin/WorldMixin.java
+++ src/main/java/net/tetro48/classicaddon/mixin/WorldMixin.java
@@ -1,13 +1,19 @@
 package net.tetro48.classicaddon.mixin;
 
+import btw.community.classicaddon.ClassicAddon;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.src.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(World.class)
 public abstract class WorldMixin {
-	@ModifyConstant(method = "<clinit>", constant = @Constant(doubleValue = 0d, ordinal = 0))
-	private static double noGloomNights(double constant) {
-		return 0.25d;
+	@Shadow private static double[] moonBrightnessByPhase;
+	@Inject(method = "computeOverworldSunBrightnessWithMoonPhases", at = @At("HEAD"))
+	private void newMoonShenanigans(CallbackInfoReturnable<Float> cir) {
+		if (ClassicAddon.isServerRunningThisAddon || MinecraftServer.getIsServer()) moonBrightnessByPhase[4] = 0.25d;
+		else moonBrightnessByPhase[4] = 0d;
 	}
 }
