b9e9e747d4aa83a457dd1afe048e66e6289e1ed4
Tetro48 <76738929+Tetro48@users.noreply.github.com>
2025-06-13 19:28:58 +0700

    fix this not working on future snapshots

--- src/main/java/net/tetro48/classicaddon/mixin/WorldMixin.java
+++ src/main/java/net/tetro48/classicaddon/mixin/WorldMixin.java
@@ -6,13 +6,14 @@ import net.minecraft.src.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(World.class)
 public abstract class WorldMixin {
 	@Shadow private static double[] moonBrightnessByPhase;
-	@Inject(method = "computeOverworldSunBrightnessWithMoonPhases", at = @At("HEAD"))
-	private void newMoonShenanigans(CallbackInfoReturnable<Float> cir) {
+	@Inject(method = "tick", at = @At("HEAD"))
+	private void newMoonShenanigans(CallbackInfo ci) {
 		if (ClassicAddon.isServerRunningThisAddon || MinecraftServer.getIsServer()) moonBrightnessByPhase[4] = 0.25d;
 		else moonBrightnessByPhase[4] = 0d;
 	}
