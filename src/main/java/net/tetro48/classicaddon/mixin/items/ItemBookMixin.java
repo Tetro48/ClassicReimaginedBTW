95c62677c4a98d552d63609a25d8642d2c241250
Tetro48 <76738929+Tetro48@users.noreply.github.com>
2025-06-14 11:19:32 +0700

    make books enchantable

---
+++ src/main/java/net/tetro48/classicaddon/mixin/items/ItemBookMixin.java
@@ -0,0 +1,15 @@
+package net.tetro48.classicaddon.mixin.items;
+
+import net.minecraft.src.ItemBook;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ItemBook.class)
+public class ItemBookMixin {
+	@Inject(method = "getItemEnchantability", at = @At("RETURN"), cancellable = true)
+	private void makeBooksEnchantable(CallbackInfoReturnable<Integer> cir) {
+		cir.setReturnValue(1);
+	}
+}
