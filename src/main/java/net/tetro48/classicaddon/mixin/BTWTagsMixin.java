891f4bdb38ae4fd8f53273a8f10f9dfcba8489aa
Tetro48 <76738929+Tetro48@users.noreply.github.com>
2025-09-06 03:49:43 +0700

    remove mystery meat from cooked meats tag in a way that is incompatible when other addons try to do the same thing

---
+++ src/main/java/net/tetro48/classicaddon/mixin/BTWTagsMixin.java
@@ -0,0 +1,16 @@
+package net.tetro48.classicaddon.mixin;
+
+import btw.item.BTWItems;
+import btw.item.tag.BTWTags;
+import net.minecraft.src.Item;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(BTWTags.class)
+public class BTWTagsMixin {
+	@ModifyArg(method = "<clinit>", index = 1, at = @At(value = "INVOKE", ordinal = 6, target = "Lbtw/item/tag/Tag;of(Lnet/minecraft/src/ResourceLocation;[Lnet/minecraft/src/Item;)Lbtw/item/tag/Tag;"))
+	private static Item[] removeMysteryMeat(Item[] items) {
+		return new Item[]{Item.beefCooked, Item.porkCooked, BTWItems.cookedMutton, BTWItems.cookedCheval, BTWItems.cookedWolfChop};
+	}
+}
