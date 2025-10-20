33a21c08dfb14da6f378def519e59890f4dbdb3e
Tetro48 <76738929+Tetro48@users.noreply.github.com>
2025-10-20 16:45:49 +0700

    Drinking milk no longer makes buckets vanish

--- src/main/java/net/tetro48/classicaddon/mixin/items/BucketItemMilkMixin.java
+++ src/main/java/net/tetro48/classicaddon/mixin/items/BucketItemMilkMixin.java
@@ -1,11 +1,15 @@
 package net.tetro48.classicaddon.mixin.items;
 
 import btw.item.items.BucketItemMilk;
+import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.Item;
+import net.minecraft.src.ItemStack;
+import net.minecraft.src.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BucketItemMilk.class)
 public abstract class BucketItemMilkMixin extends Item {
@@ -16,5 +20,14 @@ public abstract class BucketItemMilkMixin extends Item {
 	@Inject(method = "<init>", at = @At("RETURN"))
 	private void increaseStackLimit(int iItemID, CallbackInfo ci) {
 		this.maxStackSize = 16;
+	}
+	@Inject(method = "onEaten", at = @At("HEAD"))
+	private void onDrink(ItemStack itemStack, World world, EntityPlayer player, CallbackInfoReturnable<ItemStack> cir) {
+		if (itemStack.stackSize > 1) {
+			ItemStack bucket = new ItemStack(Item.bucketEmpty);
+			if (!player.inventory.addItemStackToInventory(bucket)) {
+				player.dropPlayerItem(bucket);
+			}
+		}
 	}
 }
